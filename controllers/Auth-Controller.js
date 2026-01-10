9ab8b9abe80abf3e45f0860a54e70c243ec250c7
shainti_kashyap <shaintykashyap@gmail.com>
2026-01-10 14:46:31 +0530

    Add validators

--- controllers/Auth-Controller.js
+++ controllers/Auth-Controller.js
@@ -1,3 +1,6 @@
+const { name } = require("ejs");
+const { check, validationResult } = require("express-validator");
+
 exports.Viewlogin =
   ("/Login",
   (req, res, next) => {
@@ -17,3 +20,58 @@ exports.Viewlogin =
     })
 
   })
+  exports.Getsignup=('/signup',(req, res, next)=>{
+    res.render('Partials/signup',{
+    islogedIn: false,
+    error: [],
+    oldInput: {Name: "", email: "", password: "", confirmPassword: ""}
+  });
+    })
+
+    exports.Postsignup=[
+      check("Name")
+      .trim()
+      .isLength({min: 3})
+      .withMessage("Name character should be 3 chracters"),
+
+      check("email")
+      .isEmail()
+      .withMessage("Please enter correct email"),
+
+      check("password")
+      .isLength({min: 6})
+      .withMessage("Please enter vaild password")
+      .matches(/[a-z]/)
+      .withMessage("Please use alphabets with password")
+      .matches(/[0-9]/)
+      .withMessage("Please use at least one number in password"),
+
+      check("confirmpassword")
+      .trim()
+      .custom((value, {req}) =>{
+        if(value !== req.body.password){
+          throw new Error("password do not match")
+        }
+        return true;
+      }),
+
+      // check('role')
+      // .isEmpty()
+      // .withMessage("please select one from each ")
+      // .isIn(['guest', 'host'])
+      // .withMessage("please select vaild option"),
+
+    ('/signup',(req, res, next)=>{
+     const { Name, email, password, confirmPassword} = req.body;
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+      console.log(error);
+      return res.status(422).render('partials/signup',{
+        islogedIn: false,
+        error: error.array().map(err => err.msg),
+        oldInput: {Name, email, password, confirmPassword}
+      });
+    }
+    res.redirect('/')
+    })]
+
--- routes/Auth-Router.js
+++ routes/Auth-Router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const StudentLogin = express.Router();
-const { Viewlogin, CheckLogin, Checklogout } = require('../controllers/Auth-Controller');
+const { Viewlogin, CheckLogin, Checklogout, Getsignup, Postsignup} = require('../controllers/Auth-Controller');
 
 StudentLogin.get('/Login', Viewlogin);
 StudentLogin.post('/authlogin',CheckLogin )
 StudentLogin.post("/logout",Checklogout)
+StudentLogin.get("/signup",Getsignup);
+StudentLogin.post('/signup',Postsignup);
 
 module.exports = StudentLogin;
